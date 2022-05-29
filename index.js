da6e5519cc0623bd1300063db51cf5c96026199f
John Reemar Dela Rosa <john.x.dela.rosa.-nd@disney.com>
2022-05-29 22:35:21 +0800

    added sbt transfer

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 /*** SET THESE VARIABLES ***/
-const contractAddress = ""; // Update with the address of your smart contract
-const contractAbi = ""; // Update with an ABI file, for example "./sampleAbi.json"
-
+const contractAddress = "0x4946583c5b86e01ccd30c71a05617d06e3e73060"; // Update with the address of your smart contract
+const contractAbi = "./sbtABI.json"; // Update with an ABI file, for example "./sampleAbi.json"
+const Web3 = require('web3');
+const Buffer = require('buffer/').Buffer // note: the trailing slash is important!
+const Tx = require('ethereumjs-tx').Transaction;
+const Common = require('ethereumjs-common').default;
+const bufferToHex = require('ethereumjs-util').bufferToHex;
+const privateToAddress = require('ethereumjs-util').privateToAddress;
 /*** Global scope variables that will be automatically assigned values later on ***/
 let infoSpace; // This is an <ul> element where we will print out all the info
 let web3; // Web3 instance
+let minABI;
 let contract; // Contract instance
 let account; // Your account as will be reported by Metamask
+let transferAmount;
+let destAddress;
 
 /*** Initialize when page loads ***/
 window.addEventListener("load", () => {
   // Shortcut to interact with HTML elements
   infoSpace = document.querySelector(".info");
 
-  // Check whether ethereum is defined, ie. MetaMask plugin is active
   document.querySelector(".start").addEventListener("click", async () => {
     if (contractAddress === "" || contractAbi === "") {
       printResult(
@@ -28,18 +35,105 @@ window.addEventListener("load", () => {
       );
       return;
     }
-
     // Create a Web3 instance
-    web3 = new Web3(window.ethereum);
+    web3 = new Web3(Web3.givenProvider || `http://proxy.sunblockterminal.com:7664`);
+    // web3 = new Web3(new Web3.providers.HttpProvider('http://proxy.sunblockterminal.com:7664'));
+    minABI = [
+      // transfer
+      {
+        "constant": false,
+        "inputs": [{
+            "name": "_to",
+            "type": "address"
+          },
+          {
+            "name": "_value",
+            "type": "uint256"
+          }
+        ],
+        "name": "transfer",
+        "outputs": [{
+          "name": "success",
+          "type": "bool"
+        }],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      }
+    ];
+    console.log(`web3 version: ${web3.version}`);
 
+    // Sender
+    const myAddress = "0xf96f4F26188670a0730d5F65570b6589469c8301";
+    // receiver
+    destAddress = "0xEED15870f5Bd6720C9bC3289981b885D1e0981D7";
+    // Determine  the nonce
+    const count = await web3.eth.getTransactionCount(myAddress);
+    console.log(`num transactions so far: ${count}`);
+
+    // This is the address of the contract which created the ERC20 token
+    const sbtContractAddress = "0x4946583c5b86e01ccd30c71a05617d06e3e73060";
+    const contract = new web3.eth.Contract(minABI, sbtContractAddress);
+
+
+    // await getBalance(account);
+    // await balanceOf(account);
+    // const transferAmount = 1; // This is a necessary conversion, contract methods use Wei, we want a readable Ether format
+    // listenToTransferEvent(account, destAddress, transferAmount); // Not an async function
+    // await transfer(destAddress, transferAmount);
+    const rawTransaction = {
+      "from": myAddress,
+      "nonce": "0x" + count.toString(16),
+      "gasPrice": "0x003B9ACA00",
+      "gasLimit": "0x250CA",
+      "to": sbtContractAddress,
+      "value": "0x0",
+      // "data": contract.methods.transfer(destAddress, transferAmount).encodeABI(),
+      "chainId": 5769
+    };
+
+
+    const privKey = new Buffer('0bebd15d29eafcc975266d29d906b9dbe7448df609d097be8c1bb7d1ab98bca1', 'hex');
+    const SBT_MAIN = Common.forCustomChain(
+      'mainnet', {
+        name: 'sbt',
+        networkId: 5769,
+        chainId: 5769
+      },
+      'petersburg'
+    )
+
+    const tx = new Tx(rawTransaction, {
+      common: SBT_MAIN
+    });
+    tx.sign(privKey);
+    const serializedTx = tx.serialize();
+
+    // Comment out these three lines if you don't really want to send the TX right now
+    console.log(`Attempting to send signed tx: ${serializedTx.toString('hex')}`);
+    if (
+      tx.validate() &&
+      bufferToHex(tx.getSenderAddress()) === bufferToHex(privateToAddress(privKey))
+    ) {
+      console.log('Valid signature')
+    } else {
+      console.log('Invalid signature')
+    }
+    
+    console.log("The transaction's chain id is", tx.getChainId())
+    const receipt = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'));
+    console.log(`Receipt info: ${JSON.stringify(receipt, null, '\t')}`);
+    // console.log("Transferred 1 SBT");
+    // The balance may not bu updated yet, but let's check
     // Calling desired functions
     await connectWallet();
-    await connectContract(contractAbi, contractAddress);
+    await connectContract(contractAbi, sbtContractAddress);
     await getBalance(account);
-    await balanceOf(account);
-    const transferAmount = web3.utils.toWei("1"); // This is a necessary conversion, contract methods use Wei, we want a readable Ether format
-    listenToTransferEvent(account, contractAddress, transferAmount); // Not an async function
-    await transfer(contractAddress, transferAmount);
+    // await balanceOf(account);
+    transferAmount = 1;
+    // const amount = web3.utils.toWei(value); // This is a necessary conversion, contract methods use Wei, we want a readable Ether format
+    // listenToTransferEvent(account, sbtContractAddress, transferAmount); // Not an async function
+    // await transfer(sbtContractAddress, transferAmount);
   });
 });
 
@@ -62,9 +156,11 @@ const getJson = async (path) => {
   return data;
 };
 
-// Connect to the MetaMast wallet
+// Connect to the MetaMask wallet
 const connectWallet = async () => {
-  const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+  const accounts = await ethereum.request({
+    method: "eth_requestAccounts"
+  });
   account = accounts[0];
   printResult(`Connected account: ${readableAddress(account)}`);
 };
@@ -78,17 +174,17 @@ const connectContract = async (contractAbi, contractAddress) => {
 
 // Example of a web3 method
 const getBalance = async (address) => {
-  printResult(`getBalance() requested.`);
+  // printResult(`getBalance() requested.`);
   const balance = await web3.eth.getBalance(address);
-  printResult(`Account ${readableAddress(account)} has ${web3.utils.fromWei(balance)} currency`);
+  printResult(`Account ${readableAddress(account)} has ${web3.utils.fromWei(balance)} SBT`);
 };
 
 // Example of using call() on a contract's method that doesn't require gas
 const balanceOf = async (account) => {
-  printResult(`balanceOf() called.`);
+  // printResult(`balanceOf() called.`);
   try {
     const balance = await contract.methods.balanceOf(account).call();
-    printResult(`Account ${readableAddress(account)} has ${web3.utils.fromWei(balance)} tokens.`);
+    printResult(`Account ${readableAddress(account)} has ${web3.utils.fromWei(balance)} SBT.`);
   } catch (error) {
     printResult(`Error: ${error.message}`);
   }
@@ -98,7 +194,11 @@ const balanceOf = async (account) => {
 const transfer = async (to, amount) => {
   printResult(`transfer() sent.`);
   try {
-    const result = await contract.methods.transfer(to, amount).send({ from: account });
+    const result = await contract.methods.transfer(to, amount).send({
+      from: account,
+      value: "0x0",
+      data: contract.methods.transfer(destAddress, transferAmount).encodeABI(),
+    });
     printResult(`Result: ${result.status}`);
   } catch (error) {
     printResult(`Error: ${error.message}`);
